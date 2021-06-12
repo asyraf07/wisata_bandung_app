1ab71ceed8c1ee6cca739393aa207ff4c30f33b5
asyraf07 <asyraf.sr07@gmail.com>
2021-06-12 23:54:49 +0700

    add navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wisata_bandung_app/detail_screen.dart';
+import 'package:wisata_bandung_app/main_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,8 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Wisata Bandung',
       theme: ThemeData(),
-      home: DetailScreen(),
+      home: MainScreen(),
     );
   }
 }
-
